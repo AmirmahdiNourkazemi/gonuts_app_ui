5479b3d548a4f50ab5c588f3314fd5b848b44650
Amir Mahdi Noorkazemi <nourkazemi80@gmail.com>
2024-02-15 18:03:43 +0330

    style:add photo and init screen

---
+++ lib/init_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class InitScreen extends StatefulWidget {
+  const InitScreen({super.key});
+
+  @override
+  State<InitScreen> createState() => _InitScreenState();
+}
+
+class _InitScreenState extends State<InitScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ,
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:gonuts/init_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      
+      home:InitScreen() ,
+    );
+  }
+}
